c0fb0032effcd1648ee7ad040ec1b9af4a7cce81
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-24 10:07:05 +0100

    Adding utils.py file

---
+++ ecocal/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import requests
+import io
+import pandas as pd
+from typing import Union, List
+import datetime
+import time
+from threading import Thread
+from tqdm import tqdm
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+retry = Retry(
+    total=5,
+    backoff_factor=1
+)
+adapter = HTTPAdapter(
+    max_retries=retry
+)
