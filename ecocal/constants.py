dfad355f77b03216222793d2eb45bc5fc91650cc
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-24 20:17:28 +0100

    Updating constants 2

--- ecocal/constants.py
+++ ecocal/constants.py
@@ -1,3 +1,4 @@
 API_SOURCE_URL: str = "https://calendar-api.fxstreet.com/en/api/v1/eventDates"
 BASE_URL: str = "https://www.fxstreet.com/"
 DEFAULT_USER_AGENT: str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"
+DEFAULT_THREADS: int = 20
