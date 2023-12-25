e3638112bc2e31137ea941eee96bbd6fadfb9596
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-25 17:54:45 +0100

    Updating sample script

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,18 @@
 from ecocal import *
 
 
-ec = Calendar(startHorizon="2023-10-26",
-              endHorizon="2023-11-30",
-              withDetails=True,
-              nbThreads=20,
-              preBuildCalendar=True,
-              )
-print(ec)
+def main() -> None:
+    ec = Calendar(startHorizon="2023-10-26",
+                  endHorizon="2023-11-30",
+                  withDetails=True,
+                  nbThreads=20,
+                  preBuildCalendar=True,
+                  )
+    print(ec)
+
+    # On-disk saving of detailed calendar
+    ec.saveCalendar()
+
+
+if __name__ == "__main__":
+    main()
