6fa53053edff8e5668377c3a53131c1a32ef62e8
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-25 18:17:02 +0100

    Updating setup.py for production scale

--- setup.py
+++ setup.py
@@ -10,4 +10,23 @@ PYTHON_MODULE_NAME: str = __package_name__
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setuptools.setup(
+    name=PYTHON_MODULE_NAME,
+    py_modules=[PYTHON_MODULE_NAME],
+    version=__version__,
+    author=__author__,
+    author_email="lcsrodriguez@pm.me",
+    license="MIT",
+    url=f"https://github.com/lcsrodriguez/{PYTHON_MODULE_NAME}",
+    description="Worldwide economic calendar Python package (details, estimates, market news, ...)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={'': '.'},
+    install_requires=REQUIREMENTS,  # [],
+    python_requires='>=3.10',
 )
